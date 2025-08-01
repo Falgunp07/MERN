b0d68e787c338fa62806537a52ac8ea2983d278f
Falgun <falgun@gmail.com>
2025-08-01 19:15:51 +0530

    mongobd error updated

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,20 @@
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
+const connectDB = async () => {
+  try {
+    // Event listeners for the connection
+    mongoose.connection.on('connected', () => console.log('MongoDB database connected successfully.'));
+    mongoose.connection.on('error', (err) => console.error('MongoDB connection error:', err));
+    mongoose.connection.on('disconnected', () => console.log('MongoDB database disconnected.'));
 
-//connect to database
+    // Attempt to connect to the database
+    await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`);
 
-const connectDB = async ()=>{
-    mongoose.connection.on('connected', ()=> console.log('database connected'))
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`)
-}
-export default connectDB
+export default connectDB;
