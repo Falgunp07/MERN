97382d9c17bea2662596ef1c1ad3658cba3f4cb9
Falgun <falgun@gmail.com>
2025-08-01 19:36:11 +0530

    updates

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     // Event listeners for the connection
     mongoose.connection.on('connected', () => console.log('MongoDB database connected successfully.'));
     mongoose.connection.on('error', (err) => console.error('MongoDB connection error:', err));
-    mongoose.connection.on('disconnected', () => console.log('MongoDB database disconnected.'));
+    mongoose.connection.on('disconnected', () => console.log('MongoDB database disconnected. :('));
 
     // Attempt to connect to the database
     await mongoose.connect(`${process.env.MONGODB_URI}/Edemy`);
